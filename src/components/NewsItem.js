16cc0aafd39248ff8464ccd0910a918f54eaa0f3
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2022-10-03 13:20:20 +0530

    used map to populate news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function NewsItem(props) {
   return (
-    <div>
+    <div className='my-3'>
       <div className="card" style={{width:"18rem"}}>
-  <img src="..." className="card-img-top" alt="..." />
+  <img src={props.url} className="card-img-top" alt="..." />
   <div className="card-body">
-    <h5 className="card-title">{props.title}</h5>
+    <h5 className="card-title">{props.title}...</h5>
     <p className="card-text">{props.description}</p>
-    <a href="/" className="btn btn-primary">Go somewhere</a>
+    <a href={props.newsUrl} target="_blank"   className="btn btn-sm btn-primary">Read More</a>
   </div>
 </div>
     </div>
