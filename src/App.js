e9ee5030cacf67457b6fe3f0ddbf9d3db1175d30
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2022-10-10 23:50:40 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import LoadingBar from 'react-top-loading-bar'
 export default function App() {
+  const [progress,setProgress]=useState(0);
+  const setProgressFunction=(value)=>{
+    
+    setProgress(value)
+  }
   return (
     <Router>
+     <LoadingBar
+        color='#f11946'
+        progress={progress}
+        height={3}
+        // onLoaderFinished={() => setProgressFunction(0)}
+      />
  <Navbar />
  <Routes>
-  <Route exact path='/' element={<News apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='general' />}  />
-  <Route exact path='/entertainment' element={<News apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='entertainment' />}  />
-  <Route exact path='/business' element={<News apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='business' />}  />
-  <Route exact path='/health' element={<News apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='health' />}  />
-  <Route exact path='/science' element={<News apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='science' />}  />
-  <Route exact path='/technology' element={<News apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='technology' />}  />
-  <Route exact path='/general' element={<News apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='general' />}  />
+  <Route exact path='/' element={<News setProgressFunction={setProgressFunction} apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='general' />}  />
+  <Route exact path='/entertainment' element={<News setProgressFunction={setProgressFunction}apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='entertainment' />}  />
+  <Route exact path='/business' element={<News setProgressFunction={setProgressFunction}apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='business' />}  />
+  <Route exact path='/health' element={<News setProgressFunction={setProgressFunction}apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='health' />}  />
+  <Route exact path='/science' element={<News setProgressFunction={setProgressFunction}apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='science' />}  />
+  <Route exact path='/technology' element={<News setProgressFunction={setProgressFunction}apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='technology' />}  />
+  <Route exact path='/general' element={<News setProgressFunction={setProgressFunction}apiKey='cc1c8918058e4e4e8fec69abefb33e40' country='in' category='general' />}  />
  </Routes>
    
     </Router>
--- src/components/News.js
+++ src/components/News.js
@@ -2,20 +2,27 @@ import React, { useEffect, useState } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner'
 
-export default function News({apiKey,country,category}) {
+export default function News({apiKey,country,category,setProgressFunction}) {
     const [articles,setArticles]=useState("")
-    const [page,setPage]=useState(1);
+    const [page,setPage]=useState(0);
     const [loading,setLoading]=useState(false)
+    setProgressFunction(0)
     useEffect(()=>{
+      
+      setLoading(true)
+     
       const cat=category.toUpperCase();
       document.title=`${cat} - NewsMonk`
       window.scrollTo(0, 0)
-      setLoading(true)
+      
         fetch(`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=100`).then(res=>res.json()).then(data=>{
+          setProgressFunction(100)
           setLoading(false)
-            console.log(data.articles)
+          
             setArticles(data.articles);
+            
         })
+        
     },[category])
 
     // const nextHandler=()=>{
@@ -29,7 +36,7 @@ export default function News({apiKey,country,category}) {
 
   return (
     
-    <div className='container mt-5 my-3'>
+    <div className='container mt-5 my-3' >
     <h2 className='text-center mt-5' >News Monk Top Headlines</h2>
     {loading && <Spinner /> }
     <div className="row">
@@ -44,7 +51,7 @@ export default function News({apiKey,country,category}) {
    
 
    <div className='d-flex justify-content-between mt-5'>
-   <button  type="button" class="btn btn-warning"  id='prev' onClick={previousHandler}>Go at top</button>
+   <button  type="button" className="btn btn-warning"  id='prev' onClick={previousHandler}>Go at top</button>
     {/* <button disabled={page<=1} type="button" class="btn btn-warning"  id='prev' onClick={previousHandler}>Previous</button> */}
     {/* <button type="button"  class="btn btn-warning" id='next' onClick={nextHandler}>Next</button> */}
    </div>
